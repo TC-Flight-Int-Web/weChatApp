48f35548d787f919756b420c0b6b6e8116159109
u010246479 <619599300@qq.com>
2016-09-25 22:12:13 +0800

    项目配置

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 //app.js
-App({
-    onLaunch: function () {
-        console && console.group("生命周期");
-        console && console.info('App Launch');
+App( {
+    onLaunch: function() {
+        console && console.group( "生命周期" );
+        console && console.info( 'App Launch' );
         //调用API从本地缓存中获取数据
-        var logs = wx.getStorageSync('logs') || []
-        logs.unshift(Date.now())
-        wx.setStorageSync('logs', logs)
+        var logs = wx.getStorageSync( 'logs' ) || []
+        logs.unshift( Date.now() )
+        wx.setStorageSync( 'logs', logs )
     },
-    onShow: function () {
-        console.log('App Show');
+    onShow: function() {
+        console.log( 'App Show' );
     },
-    onHide: function () {
-        console.log('App Hide');
+    onHide: function() {
+        console.log( 'App Hide' );
     },
-    getUserInfo: function (cb) {
+    getUserInfo: function( cb ) {
         var that = this;
-        if (this.globalData.userInfo) {
-            typeof cb == "function" && cb(this.globalData.userInfo)
+        if( this.globalData.userInfo ) {
+            typeof cb == "function" && cb( this.globalData.userInfo )
         } else {
             //调用登录接口
-            wx.login({
-                success: function () {
-                    wx.getUserInfo({
-                        success: function (res) {
+            wx.login( {
+                success: function() {
+                    wx.getUserInfo( {
+                        success: function( res ) {
                             that.globalData.userInfo = res.userInfo;
-                            typeof cb == "function" && cb(that.globalData.userInfo)
+                            typeof cb == "function" && cb( that.globalData.userInfo )
                         }
                     })
                 }
