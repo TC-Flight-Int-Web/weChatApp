34e9d91c3bfe4aa7c15188cb7b5cf28f2e2ab084
u010246479 <619599300@qq.com>
2016-09-25 18:54:57 +0800

    ++

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,39 @@
 //index.js
 //获取应用实例
 var app = getApp()
-Page({
+Page( {
   data: {
     motto: 'Hello World',
     userInfo: {}
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
+    wx.navigateTo( {
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
+  onLoad: function() {
+    console.log( 'index--------------------onLoad' )
     var that = this
-  	//调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo( function( userInfo ) {
       //更新数据
-      that.setData({
-        userInfo:userInfo
+      that.setData( {
+        userInfo: userInfo
       })
       that.update()
     })
+  },
+  onShow: function() {
+    console && console.info( "index--------------------onShow" );
+  },
+  onReady: function() {
+    console && console.info( "index--------------------onReady" );
+  },
+  onHide: function() {
+    console && console.info( "index--------------------onHide" );
+  },
+  onUnload: function() {
+    console && console.info( "index--------------------onUnload" );
   }
 })
